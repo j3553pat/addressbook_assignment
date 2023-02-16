a68f058b0a6bf5a6fb2256e3b65e40d188418f34
j3553pat <jessecodedev@gmail.com>
2023-02-16 12:00:03 -0600

    displayed results and organized DOM

--- main.js
+++ main.js
@@ -1,35 +1,22 @@
-
-window.onload = function() {
-    getUsers()
-  
-  }
-
-
+window.onload = function () {
+  getUsers();
+};
 
 const getUsers = () => {
-fetch('https://randomuser.me/api/?results=10')
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data)
-    displayUsers(data.results)
-  });
-}
+  fetch("https://randomuser.me/api/?results=10")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      displayUsers(data.results);
+    });
+};
 
 const displayUsers = (arrayOfUsers) => {
-    const allUsers = document.getElementById('all-users')
-    arrayOfUsers.map((user, results) => {
-      const li = document.createElement('li')
-      const text = document.createTextNode(`#${results}, Title: ${user.name}:  ${user.gender}, by user: ${user.age}`)
-      li.appendChild(text)
-      allUsers.append(li)
-      let name = document.createElement('h2');
-      let email = document.createElement('span');
-
-      name.innerHTML = `${user.name}`;
-      email.innerHTML = `${user.email}`;
-      li.appendChild(name);
-      li.appendChild(email);
-    })
-    // ul.appendChild(allUsers)
-  }
-
+  const allUsers = document.getElementById("all-users");
+  allUsers.innerHTML = "";
+  arrayOfUsers.map((user) => {
+    const li = document.createElement("li");
+    li.innerHTML = `Name: ${user.name.first}  ${user.name.last} <br> Email: ${user.email} <br> Gender:${user.gender}`;
+    allUsers.appendChild(li);
+  });
+};
