35868c9785f8bb7d6b55e7b4149b12af2d880fdc
g33zy <stevejgaz@gmail.com>
2023-02-19 14:52:20 -0600

    style

--- main.js
+++ main.js
@@ -6,22 +6,51 @@ window.onload = function() {
 }
 
 const getUsers = () => { 
-     fetch('https://randomuser.me/api/?results=10')
+     fetch('https://randomuser.me/api/?results=20')
         .then((response) => response.json())
         .then((data) => {
-            console.log(data)
-            displayUsers(data.results)
+          console.log(data)
+          displayUsers(data.results)
+            
+            
         })
+
+        
 }
 
 
 const displayUsers = (arrayOfUsers) => {
     const allUsers = document.getElementById('all-users')
-    arrayOfUsers.map((user, results) => {
+    allUsers.innerHTML = ""
+    arrayOfUsers.map((user) => {
       const li = document.createElement('li')
-      const text = document.createTextNode(`#${results}, Name: ${user.name} Gender:  ${user.gender}, email: ${user.email}`)
-      li.appendChild(text)
-      allUsers.append(li)
+      // const text = document.createTextNode(arrayOfUsers)
+      li.innerHTML = `Name: ${user.name.first} ${user.name.last}`
+
+      const userImage = document.createElement('img')
+      userImage.src = user.picture.medium
+      userImage.className = 'image'
+
+      li.appendChild(userImage)
+      allUsers.appendChild(li)
+
+      const button = document.createElement('button')
+      button.innerHTML = 'Additional Info'
+      button.onclick = () => {
+        const div = document.createElement('div')
+        div.innerHTML = `Gender:  ${user.gender} <br> Email: ${user.email} <br> Phone: ${user.phone} <br> location: ${user.location.street.number} ${user.location.street.name} ${user.location.country}`
+        li.appendChild(div)
+      }
+    
+      li.appendChild(button)
+
+      
+      
+
+      
     })
+
+    
+  
   }
 
